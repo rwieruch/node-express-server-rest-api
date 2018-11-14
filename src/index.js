8c3c3b62e46a8ca6c4098ece8c019d0c71ac0acc
Robin Wieruch <wrobin@gmx.net>
2018-11-14 10:59:41 +0800

    routing with sample data

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+app.get('/users', (req, res) => {
+  res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  res.send(users[req.params.userId]);
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
