5cea8a825977356b47e151afbac92d0c3339c847
Robin Wieruch <wrobin@gmx.net>
2018-11-14 11:43:24 +0800

    bodyparser for creating a message, application-level middleware for me user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
 import 'dotenv/config';
 import cors from 'cors';
+import uuidv4 from 'uuid/v4';
+import bodyParser from 'body-parser';
 import express from 'express';
 
 const app = express();
 
 app.use(cors());
 
+// Postman: x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Postman: raw + JSON (application/json)
+app.use(bodyParser.json());
+
+// Sample Data
+
 let users = {
   1: {
     id: '1',
     username: 'Robin Wieruch',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Dave Davids',
+    messageIds: [2],
   },
 };
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+// Application-Level Middleware
+
+app.use(function(req, res, next) {
+  req.me = users[1];
+  next();
+});
+
+// Routes
 
 app.get('/users', (req, res) => {
   res.send(Object.values(users));
@@ -23,6 +57,28 @@ app.get('/users', (req, res) => {
 
 app.get('/users/:userId', (req, res) => {
   res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+  res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  res.send(messages[req.params.messageId]);
+});
+
+app.post('/messages', ({ body: { text }, me }, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: text,
+    userId: me.id,
+  };
+
+  messages[id] = message;
+  users[me.id].messageIds.push(id);
+
+  res.send(message);
 });
 
 app.listen(process.env.PORT, () =>
