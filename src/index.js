c00c208489cd40bd0e1559ac64f91532fcadcffe
Robin Wieruch <wrobin@gmx.net>
2020-04-29 13:16:35 +0200

    uris

--- src/index.js
+++ src/index.js
@@ -6,20 +6,24 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
 });
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
 });
 
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
 });
 
 app.listen(process.env.PORT, () =>
