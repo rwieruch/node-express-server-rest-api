c3e84bb8d61599c7fec90259435fd218060a6f34
Robin Wieruch <wrobin@gmx.net>
2020-04-29 13:15:49 +0200

    http-methods

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
 });
 
 app.listen(process.env.PORT, () =>
