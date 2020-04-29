bf5ef49fb8e488dfbf9cb8f43e9c8b9b9fafd0e8
Robin Wieruch <wrobin@gmx.net>
2020-04-29 13:32:29 +0200

    modular-routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
-import uuidv4 from 'uuid/v4';
 import cors from 'cors';
 import express from 'express';
 
 import models from './models';
+import routes from './routes';
 
 const app = express();
 
@@ -20,49 +20,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/session', (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
-});
-
-app.get('/users', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
-
-app.get('/users/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
-
-  req.context.models.messages[id] = message;
-
-  return res.send(message);
-});
-
-app.delete('/messages/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
-
-  req.context.models.messages = otherMessages;
-
-  return res.send(message);
-});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
