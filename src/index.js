4cd6ca22ca278fc7e3d7435e6988a8705a6d38af
Robin Wieruch <wrobin@gmx.net>
2020-04-29 13:13:48 +0200

    Revert "init"
    This reverts commit 39f0b08ea7521687d76ebf4c1bf7168ccc601bcc.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import 'dotenv/config';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import express from 'express';
+
+import models from './models';
+import routes from './routes';
 
 const app = express();
 
+// Application-Level Middleware
+
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
+
+// Routes
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+// Start
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
