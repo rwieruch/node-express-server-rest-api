23d8312820c66137ac85ebe797c0080361f89ef4
Robin Wieruch <wrobin@gmx.net>
2020-04-29 13:21:10 +0200

    rest-sense

--- src/index.js
+++ src/index.js
@@ -6,24 +6,44 @@ const app = express();
 
 app.use(cors());
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
 app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
+  return res.send(Object.values(users));
 });
 
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
 });
 
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
 });
 
 app.listen(process.env.PORT, () =>
